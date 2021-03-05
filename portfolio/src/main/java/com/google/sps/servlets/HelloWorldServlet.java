90edef228e83c84798ae1b9fcfd2f7d4b94bf7a3
Efraín Muñoz <efrainmunoz100@gmail.com>
2021-03-05 06:32:57 +0000

    Create fetchServlet, add button to portfolio linking to servlet response, and update script to show servlet response

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -15,7 +15,7 @@ public class HelloWorldServlet extends HttpServlet {
     response.setContentType("text/html;");
     response.getWriter().println("<h1>Hello world!</h1>");
     response.getWriter().println("<p> This is Efrain coming at you live from the studio! <p>");
-    response.getWriter().println("<a href= \"/Images/sleepy_cat.jpg\"><img src=\"/Images/sleepy_cat.jpg\" style=\"width:250px;height:auto;\"/><a>");
+    response.getWriter().println("<a href= \"/Images/sleepy_cat.jpg\"><img \"src=\"/Images/sleepy_cat.jpg\" style=\"width:250px;height:auto;\"/><a>");
     response.getWriter().println("<p> This is my cat Tito, also coming at you live from the studio (but a little sleepy).<p>");
   }
 }
